33576346107fa6ad9e40d07e578b7ed288655ca2
soyebmahmud <116428048+soyebmahmud@users.noreply.github.com>
2026-08-18 02:09:35 -0400

    Add local activation configuration

---
+++ application/config/local_activation.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('BASEPATH')) {
+    exit('No direct script access allowed');
+}
+
+// Replace this hash with the bcrypt hash for the activation code used by your deployment.
+$config['local_activation_code_hash'] = '$2y$10$4xy3j0xK8xKQ8xKx4M9g9uVq4p4Qk3QvQ2j9fB4lBv2Q9jKQfG8mK';
